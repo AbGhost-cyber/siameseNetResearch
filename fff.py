3876e6fb4a2ea2b99712d309b39f600ce728f423
AbGhost-cyber <xplendo@gmail.com>
2023-07-16 15:31:03 +0800

    test

--- fff.py
+++ fff.py
@@ -12,26 +12,25 @@
 import torch
 
 from torch import nn
+from torchvision.models import densenet121
 
 cnn1 = nn.Sequential(
-    nn.Conv2d(1, 96, kernel_size=11),
-    nn.ReLU(inplace=True),
-    # nn.LocalResponseNorm(size=5, k=2, alpha=1e-4, beta=0.75),
-    nn.MaxPool2d(3, stride=2),
-    nn.Dropout(p=0.2),
+            nn.Conv2d(1, 96, kernel_size=11, stride=4),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(p=0.2),
+            nn.MaxPool2d(3, stride=2),
 
-    nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, padding_mode='zeros'),
-    nn.ReLU(inplace=True),
-    nn.MaxPool2d(2, stride=2),
-    nn.Dropout(p=0.2),
+            nn.Conv2d(96, 256, kernel_size=5, stride=1),
+            nn.ReLU(inplace=True),
+            # nn.Dropout(p=0.2),
+            nn.MaxPool2d(2, stride=2),
 
-    nn.Conv2d(256, 384, 3, stride=1, padding=1, padding_mode='zeros'),
-    nn.Conv2d(384, 256, 3, stride=1, padding=1, padding_mode='zeros'),
-    nn.MaxPool2d(2, stride=2)
-)
+            nn.Conv2d(256, 384, kernel_size=3, stride=1),
+            # nn.Dropout(p=0.2),
+            nn.ReLU(inplace=True)
+        )
 
-xx = torch.randn((1, 1, 145, 210))
+xx = torch.randn((1, 1, 224, 224))
 print(cnn1(xx).shape)
-
 if __name__ == '__main__':
     print()
