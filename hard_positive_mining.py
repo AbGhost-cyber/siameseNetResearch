b6dd944991bf9c4923cef4310f4596c4852217b3
AbGhost-cyber <xplendo@gmail.com>
2023-08-08 15:43:11 +0800

    triplet signature

--- hard_positive_mining.py
+++ hard_positive_mining.py
@@ -4,7 +4,6 @@ import numpy as np
 import torch
 import torchvision
 from PIL import Image, ImageOps
-from matplotlib import pyplot as plt
 from torch import optim
 from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets, transforms
@@ -13,6 +12,7 @@ from torchvision.datasets import ImageFolder
 import torch.nn as nn
 
 import matplotlib.pyplot as plt
+from torchvision.transforms import RandomApply, RandomChoice, RandomRotation, RandomAffine
 
 
 def imshow(img, text=None):
@@ -206,13 +206,88 @@ transformation = transforms.Compose([transforms.Resize((120, 120)),
                                      transforms.ToTensor(),
                                      transforms.Normalize(mean=[mean], std=[std])
                                      ])
-transformation_test = transforms.Compose([transforms.Resize((120, 120)),
-                                          transforms.CenterCrop(120),
-                                          transforms.Grayscale(),
-                                          transforms.ToTensor()
-                                          ])
+
+
+# transformation_test = transforms.Compose([transforms.Resize((120, 120)),
+#                                           transforms.CenterCrop(120),
+#                                           transforms.Grayscale(),
+#                                           transforms.ToTensor()
+#                                           ])
+
+# adapted from Gregory Koch
+class CustomRandomAffineWithTranslation(object):
+    def __init__(self):
+        self.rotation_range = (-10.0, 10.0)
+        self.shear_range = (-0.3, 0.3)
+        self.scale_range = (0.8, 1.2)
+        self.translate_range = (-2, 2)
+
+    def __call__(self, img):
+        angle = torch.FloatTensor(1).uniform_(self.rotation_range[0], self.rotation_range[1])
+        shear_x = torch.FloatTensor(1).uniform_(self.shear_range[0], self.shear_range[1])
+        shear_y = torch.FloatTensor(1).uniform_(self.shear_range[0], self.shear_range[1])
+        scale_x = torch.FloatTensor(1).uniform_(self.scale_range[0], self.scale_range[1])
+        scale_y = torch.FloatTensor(1).uniform_(self.scale_range[0], self.scale_range[1])
+        translate_x = torch.FloatTensor(1).uniform_(self.translate_range[0], self.translate_range[1])
+        translate_y = torch.FloatTensor(1).uniform_(self.translate_range[0], self.translate_range[1])
+
+        transform = transforms.Compose([
+            transforms.RandomAffine(degrees=(angle.item(), angle.item()),
+                                    shear=(shear_x.item(), shear_y.item()),
+                                    scale=(scale_x.item(), scale_y.item()),
+                                    translate=(translate_x.item(), translate_y.item())),
+            transforms.ToTensor()
+        ])
+
+        return transform(img)
+
+
+# Define the range of values for each component of the affine transformation
+rotation_range = (-10.0, 10.0)
+shear_range = (-0.3, 0.3)
+scale_range = (0.8, 1.2)
+total_image_width = 120
+total_image_height = 120
+translation_range = (-2 / total_image_width, 2 / total_image_height)
+translation_range_scaled = (
+    max(translation_range[0] + 1, 0),
+    min(translation_range[1] + 1, 1)
+)
+# Define the probability of including each component in the transformation
+component_prob = 0.5
+# Define the affine transform
+
+transform2 = [
+    RandomRotation(rotation_range),
+    RandomAffine(degrees=0, shear=shear_range),
+    RandomAffine(degrees=0, scale=scale_range),
+    RandomAffine(degrees=0, translate=translation_range_scaled)
+]
+
+p = [component_prob] * len(transform2)
+
+affine_transform = RandomApply([
+    RandomChoice(transform2, p=p)
+])
+
+transformation_two = transforms.Compose([transforms.Resize((120, 120)),
+                                         transforms.CenterCrop(120),
+                                         ImageOps.invert,
+                                         transforms.Grayscale(),
+                                         transforms.ToTensor(),
+                                         transforms.Normalize(mean=[mean], std=[std]),
+                                         affine_transform
+                                         ])
+test_transform = transforms.Compose([
+    transforms.Resize((120, 120)),
+    transforms.CenterCrop(120),
+    ImageOps.invert,
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[mean], std=[std])
+])
 custom_dataset = CustomDataset(root="/Users/mac/research books/signature_research/data/faces/training/",
-                               transform=transformation)
+                               transform=test_transform)
 # Load the training dataset
 train_dataloader = DataLoader(custom_dataset, shuffle=True, batch_size=64)
 
@@ -233,11 +308,62 @@ train_dataloader = DataLoader(custom_dataset, shuffle=True, batch_size=64)
 # print("std", std)
 # print("mean", mean)
 
-simple_branch = SimpleBranch()
-siamese_net = SiameseNetwork(branch=simple_branch)
-loss_fn = TripletLoss()
-optimizer = optim.Adam(siamese_net.parameters(), lr=0.0005)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2, verbose=True)
+# pixel_values = []
+#
+# for i in range(len(custom_dataset)):
+#     anchor_image, positive_image, negative_image = custom_dataset[i]
+#
+#     pixel_values.append(anchor_image)
+#     pixel_values.append(positive_image)
+#     pixel_values.append(negative_image)
+#
+# pixel_values = torch.stack(pixel_values, dim=0)
+# mean = torch.mean(pixel_values, dim=(0, 2, 3))
+# std = torch.std(pixel_values, dim=(0, 2, 3))
+#
+# mean = mean.item()
+# std = std.item()
+#
+# print("std", std)
+# print("mean", mean)
+pixel_values = []
+
+with torch.no_grad():
+    for i in range(len(custom_dataset)):
+        anchor_image, positive_image, negative_image = custom_dataset[i]
+
+        pixel_values.append(anchor_image)
+        pixel_values.append(positive_image)
+        pixel_values.append(negative_image)
+
+pixel_values = torch.stack(pixel_values, dim=0)
+mean = torch.mean(pixel_values, dim=(0, 2, 3))
+std = torch.std(pixel_values, dim=(0, 2, 3))
+
+mean = mean.item()
+std = std.item()
+
+print("std", std)
+print("mean", mean)
+# simple_branch = SimpleBranch()
+# siamese_net = SiameseNetwork(branch=simple_branch)
+# loss_fn = TripletLoss()
+# optimizer = optim.Adam(siamese_net.parameters(), lr=0.0005)
+# scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=2, verbose=True)
+#
+# # Create a simple dataloader just for simple visualization
+# vis_dataloader = DataLoader(custom_dataset,
+#                             shuffle=True,
+#                             batch_size=8)
+#
+# # Extract one batch
+# example_batch = next(iter(vis_dataloader))
+#
+# # Example batch is a list containing 2x8 images, indexes 0 and 1, and also the label
+# # If the label is 1, it means that it is not the same person, label is 0, same person in both images
+# concatenated = torch.cat((example_batch[0], example_batch[1], example_batch[2]), 0)
+#
+# imshow(torchvision.utils.make_grid(concatenated))
 
 counter = []
 loss_history = []
@@ -279,34 +405,34 @@ epoch = 20
 #
 # torch.save(siamese_net.state_dict(), "hard_positive_simple_branch.pt")
 
-test_model = SiameseNetwork(branch=simple_branch)
-state_dict = torch.load('hard_positive_simple_branch.pt')
-test_model.load_state_dict(state_dict)
-test_model.eval()
-
-# Locate the test dataset and load it into the SiameseNetworkDataset
-custom_dataset1 = CustomDataset(root="/Users/mac/research books/signature_research/data/faces/testing/",
-                                transform=transformation)
-test_dataloader = DataLoader(custom_dataset1, batch_size=1, shuffle=True)
-
-# Grab one image that we are going to test
-dataiter = iter(test_dataloader)
-anchor_image, _, _ = next(dataiter)
-
-for i in range(15):
-    # Iterate over 5 images and test them with the first image (x0)
-    _, positive_image, negative_image = next(dataiter)
-
-    # Concatenate the two images together
-    concatenated_positive = torch.cat((anchor_image, positive_image, negative_image), 0)
-
-    output_anchor, output_positive, output_negative = test_model(anchor_image, positive_image, negative_image)
-    euclidean_distance_positive = torch.nn.functional.pairwise_distance(output_anchor, output_positive)
-    euclidean_distance_negative = torch.nn.functional.pairwise_distance(output_anchor, output_negative)
-    imshow(torchvision.utils.make_grid(concatenated_positive),
-           f'Dissimilarity b/w Anchor and Positive: {euclidean_distance_positive.item():.2f}\n'
-           f'Dissimilarity b/w Anchor and Negative: {euclidean_distance_negative.item():.2f}'
-           )
+# test_model = SiameseNetwork(branch=simple_branch)
+# state_dict = torch.load('hard_positive_simple_branch.pt')
+# test_model.load_state_dict(state_dict)
+# test_model.eval()
+#
+# # Locate the test dataset and load it into the SiameseNetworkDataset
+# custom_dataset1 = CustomDataset(root="/Users/mac/research books/signature_research/data/faces/testing/",
+#                                 transform=transformation)
+# test_dataloader = DataLoader(custom_dataset1, batch_size=1, shuffle=True)
+#
+# # Grab one image that we are going to test
+# dataiter = iter(test_dataloader)
+# anchor_image, _, _ = next(dataiter)
+#
+# for i in range(15):
+#     # Iterate over 5 images and test them with the first image (x0)
+#     _, positive_image, negative_image = next(dataiter)
+#
+#     # Concatenate the two images together
+#     concatenated_positive = torch.cat((anchor_image, positive_image, negative_image), 0)
+#
+#     output_anchor, output_positive, output_negative = test_model(anchor_image, positive_image, negative_image)
+#     euclidean_distance_positive = torch.nn.functional.pairwise_distance(output_anchor, output_positive)
+#     euclidean_distance_negative = torch.nn.functional.pairwise_distance(output_anchor, output_negative)
+#     imshow(torchvision.utils.make_grid(concatenated_positive),
+#            f'Dissimilarity b/w Anchor and Positive: {euclidean_distance_positive.item():.2f}\n'
+#            f'Dissimilarity b/w Anchor and Negative: {euclidean_distance_negative.item():.2f}'
+#            )
 
 if __name__ == '__main__':
     print()
---
+++ triplet_signature.py
@@ -0,0 +1,280 @@
+import random
+from shutil import move
+
+import numpy as np
+import torch
+import torchvision
+from PIL import Image, ImageOps
+from torch import optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import datasets, transforms
+import math
+from torchvision.datasets import ImageFolder
+import torch.nn as nn
+import os
+from PIL import Image
+import matplotlib.pyplot as plt
+from torchvision.transforms import RandomApply, RandomChoice, RandomRotation, RandomAffine
+
+
+def imshow(img, text=None):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic', fontweight='bold',
+                 bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 10})
+
+    plt.imshow(np.transpose(npimg, (1, 2, 0)))
+    plt.show()
+
+
+# def select_images(source_folder, destination_folder, num_images_per_index=12):
+#     # Create the destination folder if it doesn't exist
+#     if not os.path.exists(destination_folder):
+#         os.makedirs(destination_folder)
+#
+#     # Iterate over the files in the source folder
+#     for filename in os.listdir(source_folder):
+#         if not filename.__contains__("png"):
+#             continue
+#         # Split the filename into parts
+#         parts = filename.split('_')
+#
+#         # Extract the index and sub-index
+#         index = int(parts[1])
+#         sub_index = int(parts[2].split('.')[0])
+#
+#         # Check if the current index and sub-index meet the criteria
+#         if sub_index <= num_images_per_index:
+#             # Construct the source and destination paths
+#             source_path = os.path.join(source_folder, filename)
+#             destination_path = os.path.join(destination_folder, filename)
+#
+#             # Copy the image to the destination folder
+#             shutil.copyfile(source_path, destination_path)
+#
+#
+# # Specify the source and destination folders and the number of images per index
+# full_org_folder = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_org'
+# full_forg_folder = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_forg'
+# new_full_org_folder = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_new_org'
+# new_full_forg_folder = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_new_forg'
+
+# # Select and copy the desired images for genuine signatures
+# select_images(full_org_folder, new_full_org_folder)
+#
+# # Select and copy the desired images for forged signatures
+# select_images(full_forg_folder, new_full_forg_folder)
+
+class CustomDataset(ImageFolder):
+    def __init__(self, root, transform=None):
+        super().__init__(root, transform=transform)
+        self.targets = torch.tensor(self.targets)  # Convert targets to tensor for indexing
+
+    def get_hard_triplet(self, anchor_index):
+        anchor_label = self.targets[anchor_index]
+
+        # Find positive samples with the same label as the anchor
+        positive_indices = (self.targets == anchor_label).nonzero().flatten()
+        positive_indices = positive_indices[positive_indices != anchor_index]
+
+        # Find negative samples with different labels
+        negative_indices = (self.targets != anchor_label).nonzero().flatten()
+
+        # Select the hardest positive sample
+        hardest_positive_index = self.get_hardest_sample(anchor_index, positive_indices)
+
+        # Select a random negative sample
+        hardest_negative_index = self.get_hardest_sample(anchor_index, negative_indices)
+
+        # Load the images
+        anchor_image_path = self.samples[anchor_index][0]
+        positive_image_path = self.samples[hardest_positive_index][0]
+        negative_image_path = self.samples[hardest_negative_index][0]
+
+        anchor_image = Image.open(anchor_image_path)
+        positive_image = Image.open(positive_image_path)
+        negative_image = Image.open(negative_image_path)
+
+        if self.transform is not None:
+            anchor_image = self.transform(anchor_image)
+            positive_image = self.transform(positive_image)
+            negative_image = self.transform(negative_image)
+
+        return anchor_image, positive_image, negative_image
+
+    def get_hardest_sample(self, anchor_index, indices):
+        anchor_path, _ = self.samples[anchor_index]
+        anchor_features = Image.open(anchor_path)
+        if self.transform is not None:
+            anchor_features = self.transform(anchor_features)
+        distances = []
+        for index in indices:
+            features, _ = self.samples[index]
+            features = Image.open(features)
+            if self.transform is not None:
+                features = self.transform(features)
+                distance = torch.norm(anchor_features - features)
+                distances.append(distance.item())
+
+        hardest_index = indices[distances.index(max(distances))]
+        return hardest_index
+
+    def __getitem__(self, index):
+        return self.get_hard_triplet(anchor_index=index)
+
+    def __len__(self):
+        return len(self.samples)
+
+
+class SignatureDataset(Dataset):
+    def __init__(self, genuine_folder, forged_folder, transform=None):
+        self.genuine_dataset = CustomDataset(genuine_folder, transform=transform)
+        self.forged_dataset = CustomDataset(forged_folder, transform=transform)
+        self.transform = transform
+
+    def get_hard_triplet(self, anchor_index):
+        anchor_image, positive_image, negative_image = self.genuine_dataset.get_hard_triplet(anchor_index)
+
+        # Find the hardest negative sample from the forged dataset
+        hardest_negative_index = self.forged_dataset.get_hardest_sample(anchor_index)
+
+        # Load the images from the forged dataset
+        negative_image_path = self.forged_dataset.samples[hardest_negative_index][0]
+        negative_image = Image.open(negative_image_path)
+        if self.transform is not None:
+            negative_image = self.transform(negative_image)
+
+        return anchor_image, positive_image, negative_image
+
+    def get_hardest_sample(self, anchor_index):
+        return self.genuine_dataset.get_hardest_sample(anchor_index)
+
+    def __getitem__(self, index):
+        return self.get_hard_triplet(anchor_index=index)
+
+    def __len__(self):
+        return len(self.genuine_dataset)
+
+
+class SignatureDataset2(Dataset):
+    def __init__(self, genuine_folder, forged_folder, transform=None):
+        self.genuine_dataset = CustomDataset(genuine_folder, transform=transform)
+        self.forged_dataset = CustomDataset(forged_folder, transform=transform)
+        self.transform = transform
+
+    def get_hard_triplet(self, anchor_index):
+        anchor_image, positive_image, negative_image = self.genuine_dataset.get_hard_triplet(anchor_index)
+
+        # Find the hardest negative sample from the forged dataset
+        # hardest_negative_index = self.get_hardest_sample(anchor_index, self.forged_dataset)
+
+        # Load the images from the forged dataset
+        negative_image_path = self.forged_dataset.samples[anchor_index][0]
+        negative_image = Image.open(negative_image_path)
+        if self.transform is not None:
+            negative_image = self.transform(negative_image)
+
+        return anchor_image, positive_image, negative_image
+
+    def get_hardest_sample(self, anchor_index, dataset):
+        anchor_path, _ = dataset.samples[anchor_index]
+        anchor_image = Image.open(anchor_path)
+        if self.transform is not None:
+            anchor_image = self.transform(anchor_image)
+
+        # Extract features using a pre-trained model
+        model = torchvision.models.resnet50(pretrained=True)
+        # change to accept grayscale
+        model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        model.eval()
+        with torch.no_grad():
+            anchor_features = model(torch.unsqueeze(anchor_image, 0))
+
+            distances = []
+            for index in range(len(dataset)):
+                if index != anchor_index:
+                    image_path, _ = dataset.samples[index]
+                    image = Image.open(image_path)
+                    if self.transform is not None:
+                        image = self.transform(image)
+                    features = model(torch.unsqueeze(image, 0))
+                    distance = torch.norm(anchor_features - features)
+                    distances.append(distance.item())
+
+        hardest_index = distances.index(max(distances))
+        return hardest_index
+
+    def __getitem__(self, index):
+        return self.get_hard_triplet(anchor_index=index)
+
+    def __len__(self):
+        return len(self.genuine_dataset)
+
+
+# def organize_images_into_subfolders(root_folder):
+#     for filename in os.listdir(root_folder):
+#         if filename.endswith(".png"):
+#             parts = filename.split("_")
+#             original_index = int(parts[1])
+#             folder_name = f"s{original_index}"
+#             folder_path = os.path.join(root_folder, folder_name)
+#
+#             os.makedirs(folder_path, exist_ok=True)
+#             file_path = os.path.join(root_folder, filename)
+#             new_file_path = os.path.join(folder_path, filename)
+#             move(file_path, new_file_path)
+#
+#
+# original_signatures_root = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_new_org'
+# forg_signatures_root = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_new_forg'
+# # Usage example:
+# organize_images_into_subfolders(original_signatures_root)
+# organize_images_into_subfolders(forg_signatures_root)
+# print("done")
+# std 0.20561213791370392
+# mean 0.5613707900047302
+transformation = transforms.Compose([
+    transforms.Resize((120, 120)),
+    transforms.CenterCrop(120),
+    ImageOps.invert,
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+    # transforms.Normalize(mean=[mean], std=[std])
+])
+
+original_signatures_root = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_new_org'
+forg_signatures_root = '/Users/mac/PycharmProjects/signetTest/data/CEDAR/full_new_forg'
+
+train_dataset = SignatureDataset2(genuine_folder=original_signatures_root, forged_folder=forg_signatures_root,
+                                  transform=transformation)
+# Load the training dataset
+train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=64)
+
+# example_batch = next(iter(train_dataloader))
+# concatenated = torch.cat((example_batch[0], example_batch[1], example_batch[2]), 0)
+# imshow(torchvision.utils.make_grid(concatenated))
+
+# pixel_values = []
+#
+# with torch.no_grad():
+#     for i in range(len(train_dataset)):
+#         print(f"going for {i + 1}")
+#         anchor_image, positive_image, negative_image = train_dataset[i]
+#
+#         pixel_values.append(anchor_image)
+#         pixel_values.append(positive_image)
+#         pixel_values.append(negative_image)
+#
+# pixel_values = torch.stack(pixel_values, dim=0)
+# mean = torch.mean(pixel_values, dim=(0, 2, 3))
+# std = torch.std(pixel_values, dim=(0, 2, 3))
+#
+# mean = mean.item()
+# std = std.item()
+#
+# print("std", std)
+# print("mean", mean)
+
+if __name__ == '__main__':
+    print()
