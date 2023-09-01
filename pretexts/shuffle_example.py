10e53f22c489b5f346a2b5c8327f229600d7fd37
AbGhost-cyber <xplendo@gmail.com>
2023-09-01 18:15:01 +0800

    pretext

--- pretexts/shuffle_example.py
+++ pretexts/shuffle_example.py
@@ -1,58 +1,56 @@
+import random
+
 import torch
-import torchvision.transforms.functional as F
-from matplotlib import pyplot as plt
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+import torchvision.transforms as transforms
 from PIL import Image
 
-import torch
-from torchvision import transforms
 
-
-def shuffle_patches(image, patch_size):
-    transform = transforms.Compose([
-        transforms.RandomCrop(patch_size),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomVerticalFlip()
+def extract_patches(original):
+    image_dimension = 224
+    num_division = 3
+    patch_size = image_dimension // num_division
+    crop_64 = transforms.RandomCrop(64)  # Crop patches of size 64x64
+    color_transform = transforms.Grayscale()  # Convert the patches to grayscale
+    final_transform = transforms.Compose([
+        crop_64,
+        color_transform,
+        transforms.ToTensor()  # Convert the patches to tensors
     ])
-    print(f"original image length: {len(image)}")
-    print(f"original image shape: {image.shape}")
 
-    patches = []
-    for _ in range(len(image)):
-        patch = transform(image)
-        patches.append(patch)
-    shuffled_image = torch.cat(patches, dim=0)
-    # shuffled_image = shuffled_image.view(-1, 3, patch_size, patch_size)
-    print(f"original shuffled image length: {len(shuffled_image)}")
-    print(f"original shuffled image shape: {shuffled_image.shape}")
-    return shuffled_image
+    # Crop a sample from the original image
+    sample = transforms.RandomCrop(image_dimension + 1)(original)
+
+    # Define the cropped areas based on the patch size
+    cropped_areas = []
+    for i in range(num_division):
+        for j in range(num_division):
+            # Calculate the coordinates for cropping each patch
+            cropped_area = (i * patch_size, j * patch_size, (i + 1) * patch_size, (j + 1) * patch_size)
+            cropped_areas.append(cropped_area)
+
+    # Crop the patches from the sample
+    samples = [sample.crop(cropped_area) for cropped_area in cropped_areas]
+
+    # Apply the final transformations to each patch
+    samples = [final_transform(patch) for patch in samples]
+
+    # Shuffle the patches randomly
+    random.shuffle(samples)
+
+    return samples
 
 
-original_img = Image.open("/Users/mac/Downloads/samia.jpeg")
-original_img_tensor = transforms.ToTensor()(original_img)
-patch_size = 32
-shuffled_img_tensor = shuffle_patches(original_img_tensor, patch_size)
-num_patches = shuffled_img_tensor.shape[0]
-patch_index = 0
-selected_patch_tensor = shuffled_img_tensor[patch_index]
-selected_patch_array = selected_patch_tensor.numpy().transpose(1, 2, 0)
-print(selected_patch_array.size())
-plt.imshow(selected_patch_array)
-plt.axis('off')
+# Example usage
+image = Image.open("/Users/mac/Downloads/samia.jpeg")
+extracted_patches = extract_patches(image)
+print(len(extracted_patches))
+fig, axes = plt.subplots(3, 3, figsize=(7, 7))
+for i, ax in enumerate(axes.flat):
+    patch = extracted_patches[i].permute(1, 2, 0)  # Transpose the dimensions for plotting
+    print(f"patch shape {patch.shape}")
+    ax.imshow(patch, cmap='gray')
+    ax.axis('off')
+
 plt.show()
-
-# num_subplots = min(num_patches, 9)  # Set the maximum number of subplots to 9
-# shuffled_img = shuffled_img_tensor.view(num_patches // 3, 3, patch_size, patch_size)
-# print(f"size {shuffled_img.size()}")
-# shuffled_img = shuffled_img.permute(0, 2, 3, 1).numpy()
-#
-# # Visualize the shuffled image
-# fig, axs = plt.subplots(3, num_patches // 3, figsize=(num_patches // 3, 3))
-# for i, ax in enumerate(axs.flat):
-#     if i < 3:
-#         ax.imshow(shuffled_img[i])
-#     ax.axis('off')
-#
-# plt.show()
-
-if __name__ == '__main__':
-    print()
